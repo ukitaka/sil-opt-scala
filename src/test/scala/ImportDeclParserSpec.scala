526e4cb2f41482ed7a4612e2dc60301f7f0767a2
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-05 22:55:49 +0900

    Refacator tests

--- src/test/scala/ImportDeclParserSpec.scala
+++ src/test/scala/ImportDeclParserSpec.scala
@@ -2,8 +2,8 @@ import me.waft.parser.ParseImportDecl
 import me.waft.sil.decl.ImportDecl
 import org.scalatest._
 
-class ParseImportDeclSpec extends FlatSpec with Matchers {
-  "parseImportDecl" should "work well" in {
+class ImportDeclParserSpec extends FlatSpec with Matchers {
+  "ImportDecl parser" should "work well" in {
     val sil = "import SwiftShims"
     val result = ParseImportDecl.importDecl.parse(sil).get.value
     result should be (ImportDecl("SwiftShims"))
--- src/test/scala/SILStageDeclParserSpec.scala
+++ src/test/scala/SILStageDeclParserSpec.scala
@@ -2,14 +2,14 @@ import me.waft.parser.ParseSILStageDecl
 import me.waft.sil.decl.SILStageDecl
 import org.scalatest._
 
-class ParseSILStageDeclSpec extends FlatSpec with Matchers {
-  "parseSILStageDecl" should "work well with sil_stage canonical" in {
+class SILStageDeclParserSpec extends FlatSpec with Matchers {
+  "SILStageDecl parser" should "work well with sil_stage canonical" in {
     val sil = "sil_stage canonical"
     val result = ParseSILStageDecl.silStageDecl.parse(sil).get.value
     result should be (SILStageDecl.Canonical)
   }
 
-  "parseSILStageDecl" should "work well with sil_stage raw" in {
+  "SILStageDecl parser" should "work well with sil_stage raw" in {
     val sil = "sil_stage raw"
     val result = ParseSILStageDecl.silStageDecl.parse(sil).get.value
     result should be (SILStageDecl.Raw)
