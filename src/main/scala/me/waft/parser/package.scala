4fdc3c955a92c5f7784057ebb0f71b2115b0a426
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-05 21:24:42 +0900

    Add ImportDecl parser spec

--- src/main/scala/me/waft/parser/package.scala
+++ src/main/scala/me/waft/parser/package.scala
@@ -4,6 +4,6 @@ import fastparse.all._
 
 package object parser {
   def upper = CharIn('A' to 'Z')
-  def lower = CharIn('a' to 'b')
+  def lower = CharIn('a' to 'z')
   def digit = CharIn('0' to '9')
 }
---
+++ src/test/scala/ParseImportDeclSpec.scala
@@ -0,0 +1,11 @@
+import me.waft.parser.{ParseImportDecl, ParseSILStageDecl}
+import me.waft.sil.decl.ImportDecl
+import org.scalatest._
+
+class ParseImportDeclSpec extends FlatSpec with Matchers {
+  "parseImportDecl" should "work well" in {
+    val sil = "import SwiftShims"
+    val result = ParseImportDecl.importDecl.parse(sil).get.value
+    result should be (ImportDecl("SwiftShims"))
+  }
+}
