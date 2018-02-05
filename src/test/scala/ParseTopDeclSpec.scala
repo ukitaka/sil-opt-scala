d2ae08cd6788f9a2ddb31867c475e9538235440c
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-05 18:43:59 +0900

    Add test for parse top decl

---
+++ src/test/scala/ParseTopDeclSpec.scala
@@ -0,0 +1,7 @@
+import org.scalatest._
+
+class ParseTopDeclSpec extends FlatSpec with Matchers {
+  "parseTopDecl" should "work well" in {
+    //TODO
+  }
+}
--- src/test/scala/SILStageDeclSpec.scala
+++ src/test/scala/SILStageDeclSpec.scala
@@ -3,12 +3,12 @@ import me.waft.sil.decl.SILStageDecl
 import org.scalatest._
 
 class SILStageDeclSpec extends FlatSpec with Matchers {
-  "isRaw" should "works well" in {
+  "isRaw" should "work well" in {
     SILStageDecl.Raw.isRaw should be (true)
     SILStageDecl.Canonical.isRaw should be (false)
   }
 
-  "isCanonical" should "works well" in {
+  "isCanonical" should "work well" in {
     SILStageDecl.Raw.isCanonical should be (false)
     SILStageDecl.Canonical.isCanonical should be (true)
   }
