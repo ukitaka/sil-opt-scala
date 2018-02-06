335b8da7cfa1d4e077a429277e0a94dcf2105e4a
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-06 17:13:40 +0900

    Add spec

--- src/test/scala/SILBasicBlockParserSpec.scala
+++ src/test/scala/SILBasicBlockParserSpec.scala
@@ -1,13 +1,16 @@
 import me.waft.parser.SILBasicBlockParser
+import me.waft.sil._
 import org.scalatest._
 
 class SILBasicBlockParserSpec extends FlatSpec with Matchers {
   "SIL basic block parser" should "work well" in {
     val sil =
       """|bb0(%0 : $Dog):
-         |  return %0 : $Dog
+         |  return %1 : $Dog
        """.stripMargin
     val result = SILBasicBlockParser.basicBlock.parse(sil).get.value
     result.label.identifier should be ("bb0")
+    result.label.args.head should be (SILArgument("%0", SILType("$Dog")))
+    result.terminator should be (Return(SILOperand(SILValue("%1"), SILType("$Dog"))))
   }
 }
