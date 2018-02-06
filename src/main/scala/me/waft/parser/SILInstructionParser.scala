d3691028b30b9471357466388ea41d0b37adb87b
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-06 15:07:02 +0900

    Fix SIL Instruction parsre

--- src/main/scala/me/waft/parser/SILInstructionParser.scala
+++ src/main/scala/me/waft/parser/SILInstructionParser.scala
@@ -1,13 +1,24 @@
 package me.waft.parser
 
-import fastparse.all._
+import fastparse.noApi._
+import White._
 import SILValueParser._
+import me.waft.sil.{SILArgument, SILValue}
 
 object SILInstructionParser {
-    def silInstructionResult: P[Seq[String]] =
-        ("(" ~ silValueName ~ silInstructionResultNames ~ ")").map(names => names._1 +: names._2)
+  def silInstructionResult: P[Seq[SILValue]] =
+    ("(" ~ silValueName ~ silInstructionResultNames ~ ")")
+      .map(names => names._1 +: names._2)
+      .map(_.map(SILValue.apply))
 
-    private[this] def silInstructionResultNames: P[Seq[String]] = {
-        ("," ~ silValueName).rep(0).?.map(_.getOrElse(Seq.empty))
-    }
+  private[this] def silInstructionResultNames: P[Seq[String]] = {
+    ("," ~ silValueName).rep(0).?.map(_.getOrElse(Seq.empty))
+  }
+
+  private[this] def silInstructionSourceInfo: P[String] = ???
+
+  def silInstruction: P[String] = ???
+
+  //FIXME
+  def silInstructionDef: P[String] = ((silInstructionResult ~ "=").? ~ silInstruction ~ silInstructionSourceInfo).!
 }
