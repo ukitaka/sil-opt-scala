6933359643ecac42c9f4a98c85931ef7a99dde95
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-06 12:36:37 +0900

    Add Identifier parser spec

---
+++ src/test/scala/IdentifierParserSpec.scala
@@ -0,0 +1,16 @@
+import me.waft.parser.{CommentParser, IdentifierParser}
+import org.scalatest._
+
+class IdentifierParserSpec extends FlatSpec with Matchers {
+  "SIL Identifier parser" should "work well" in {
+    val identifier = "abc1234 "
+    val result = IdentifierParser.SIL.identifier.parse(identifier).get.value
+    result should be ("abc1234")
+  }
+
+  "Swift Identifier parser" should "work well" in {
+    val identifier = "abc1234 "
+    val result = IdentifierParser.Swift.identifier.parse(identifier).get.value
+    result should be ("abc1234")
+  }
+}
