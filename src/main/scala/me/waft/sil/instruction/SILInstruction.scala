2cd3e257fbfa261d8fefab8ca5ff6d5bdabb553d
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-07 22:37:31 +0900

    Add DeclRef

--- src/main/scala/me/waft/sil/instruction/SILInstruction.scala
+++ src/main/scala/me/waft/sil/instruction/SILInstruction.scala
@@ -1,9 +1,11 @@
 package me.waft.sil.instruction
 
-import me.waft.sil.SILType
+import me.waft.sil.{SILOperand, SILType}
 
 sealed abstract class SILInstruction(name: String)
 
 case class AllocStack(`type`: SILType) extends SILInstruction("alloc_stack")
 
 case class AllocBox(`type`: SILType) extends SILInstruction("alloc_box")
+
+case class StructExtract(operand: SILOperand) extends SILInstruction("struct_extract")
