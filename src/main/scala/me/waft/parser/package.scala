a3b8e5cb18b72b722ecf04ab00502e949e8db993
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-09 14:03:05 +0900

    Refactor

--- src/main/scala/me/waft/parser/package.scala
+++ src/main/scala/me/waft/parser/package.scala
@@ -15,22 +15,21 @@ package object parser {
 
   private val WL0 = NoTrace(whitespaces)
 
-  class WhitespaceApi2[+T](p0: P[T], WL: P0) extends fastparse.WhitespaceApi[T](p0, WL) {
+  class WhitespaceApi0[+T](p0: P[T], WL: P0) extends fastparse.WhitespaceApi[T](p0, WL) {
     def repTC[R](min: Int = 0, max: Int = Int.MaxValue, exactly: Int = -1)
                 (implicit ev: fastparse.core.Implicits.Repeater[T, R]): P[R] =
       rep[R](min, ",", max, exactly) ~ trailingComma
   }
 
-  class WhitespaceApiForSeq[+T](p0: P[Seq[T]], WL: P0) extends fastparse.WhitespaceApi[Seq[T]](p0, WL) {
+  class WhitespaceApiSeq[+T](p0: P[Seq[T]], WL: P0) extends fastparse.WhitespaceApi[Seq[T]](p0, WL) {
     def ?? = ?.map(_.getOrElse(Seq.empty))
   }
 
   class Wrapper(WL: P0){
-    implicit def parserApi2[T, V](p0: T)(implicit c: T => P[V]): WhitespaceApi2[V] =
-      new WhitespaceApi2[V](p0, WL)
-
-    implicit def parserForSeq[T](p0: P[Seq[T]]): WhitespaceApiForSeq[T] =
-      new WhitespaceApiForSeq[T](p0, WL)
+    implicit def parserApi[T, V](p0: T)(implicit c: T => P[V]): WhitespaceApi0[V] =
+      new WhitespaceApi0[V](p0, WL)
+    implicit def parserForSeq[T](p0: P[Seq[T]]): WhitespaceApiSeq[T] =
+      new WhitespaceApiSeq[T](p0, WL)
   }
 
   val WhiteSpaceApi = new Wrapper(WL0)
