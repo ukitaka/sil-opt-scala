70d04ee87bdd4ae81c01e4d224da6e5c764e6268
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-09 11:41:39 +0900

    Organize comment parser spec

--- src/test/scala/me/waft/parser/CommentParserSpec.scala
+++ src/test/scala/me/waft/parser/CommentParserSpec.scala
@@ -1,22 +1,21 @@
 package me.waft.parser
 
-import me.waft.sil.instruction.AllocStack
+import me.waft.parser.White._
 import me.waft.sil._
 import me.waft.swift.`type`.NominalType
 import org.scalatest._
-import White._
 
 class CommentParserSpec extends FlatSpec with Matchers {
-  "Comment parser" should "work well" in {
-    val sil = "// here is comment\n"
-    val result = CommentParser.comment.!.parse(sil).get.value
-    result should be ("// here is comment\n")
-  }
-
   "Whitespaces parser" should "work well" in {
     val sil = " // here is comment\n // here is also comment"
     val result = whitespaces.!.parse(sil).get.value
     result should be (" // here is comment\n // here is also comment")
+  }
+
+  "Comment parser" should "work well" in {
+    val sil = "// here is comment\n"
+    val result = CommentParser.comment.!.parse(sil).get.value
+    result should be ("// here is comment\n")
   }
 
   "Parser" should "work well with comment" in {
@@ -28,9 +27,6 @@ class CommentParserSpec extends FlatSpec with Matchers {
     val result = SILBasicBlockParser.basicBlock.parse(sil).get.value
     val dog = NominalType("Dog")
     result.label.identifier should be ("bb0")
-    result.label.args.head should be (SILArgument("%0", SILType(dog)))
-    result.instructionDefs.head.instruction should be (AllocStack(SILType(dog)))
-    result.instructionDefs.head.values.head should be (SILValue("%1"))
     result.terminator should be (Return(SILOperand(SILValue("%1"), SILType(dog))))
   }
 }
