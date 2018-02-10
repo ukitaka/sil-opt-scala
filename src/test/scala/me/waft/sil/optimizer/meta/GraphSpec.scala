fc8ed27b84fd00f9cf9d7e153d001b006aea77c8
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-10 18:16:55 +0900

    Add spec

--- src/test/scala/me/waft/sil/optimizer/meta/GraphSpec.scala
+++ src/test/scala/me/waft/sil/optimizer/meta/GraphSpec.scala
@@ -2,15 +2,16 @@ package me.waft.sil.optimizer.meta
 
 import org.scalatest._
 
-import scalax.collection.GraphPredef._
 import scalax.collection.Graph
+import scalax.collection.GraphPredef._
 
 class GraphSpec extends FlatSpec with Matchers {
   // %0 = …
   // %1 = …
   // %2 = …
   // %3 = op %1, %2
-  // %4 = op %2, %3
+  // %4 = op %2
+  // %5 = op %2
   "graph" should "work well" in {
     val g = Graph(0, 3~>1, 3~>2, 4~>2, 5~>2)
     g.find(4) should be(Some(4))
@@ -21,5 +22,8 @@ class GraphSpec extends FlatSpec with Matchers {
     e.adjacents should be(Set(3 ~> 2, 5 ~> 2))
 
     g.filter((i: Int) => i % 2 == 0) should be(Graph(0, 4 ~> 2))
+    g.filter(g.having(_ == 2)) should be(Graph(2))
+    g.filter(g.having(node = _ == 2)) should be(Graph(2))
+    g.filter(g.having(edge = _ contains 2)) should be(Graph(3~>2, 4~>2, 5~>2))
   }
 }
