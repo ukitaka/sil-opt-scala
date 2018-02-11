f4af2d7b009550ee106c4245ad15f860023a1fec
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-11 19:25:56 +0900

    Add unusedArgs

--- src/main/scala/me/waft/sil/optimizer/meta/SILValueUsage.scala
+++ src/main/scala/me/waft/sil/optimizer/meta/SILValueUsage.scala
@@ -16,6 +16,14 @@ case class SILValueUsage(bb: SILBasicBlock) {
   def valueDecl(value: SILValue): Option[SILInstructionDef] =
     bb.instructionDefs.filter(_.values.contains(value)).headOption
 
+  lazy val unusedArgs: Set[SILValue] =
+    usageGraph.nodes
+      .filter(node =>
+        bb.label.args.exists(_.value == node.value)
+          && usageGraph.filter(usageGraph.having(edge = _.target == node)).isEmpty
+      )
+      .map(_.value)
+
   lazy val unusedValues: Set[SILValue] =
     usageGraph.nodes
       .filter(node =>
--- src/test/scala/me/waft/sil/optimizer/meta/SILValueUsageSpec.scala
+++ src/test/scala/me/waft/sil/optimizer/meta/SILValueUsageSpec.scala
@@ -10,26 +10,28 @@ import scalax.collection.GraphPredef._
 class SILValueUsageSpec extends FlatSpec with Matchers with SILBasicBlockParser {
   "SIL Value usage" should "be analysed well" in {
     val sil =
-      """|bb0(%0 : $Bool):
-         |  %1 = struct_extract %0 : $Bool, #Bool._value
-         |  %2 = struct_extract %0 : $Bool, #Bool._value
-         |  %3 = integer_literal $Builtin.Int1, -1
-         |  %4 = builtin "xor_Int1"(%2 : $Builtin.Int1, %3 : $Builtin.Int1) : $Builtin.Int1
-         |  %5 = struct $Bool (%4 : $Builtin.Int1)
-         |  return %5 : $Bool
+      """|bb0(%0 : $Bool, %1 : $Bool):
+         |  %2 = struct_extract %1 : $Bool, #Bool._value
+         |  %3 = struct_extract %1 : $Bool, #Bool._value
+         |  %4 = integer_literal $Builtin.Int1, -1
+         |  %5 = builtin "xor_Int1"(%3 : $Builtin.Int1, %4 : $Builtin.Int1) : $Builtin.Int1
+         |  %6 = struct $Bool (%5 : $Builtin.Int1)
+         |  return %6 : $Bool
       """.stripMargin
     val bb = basicBlock.parse(sil).get.value
     val usage = SILValueUsage(bb)
     val graph = usage.analyseUsages(bb)
 
     graph should be(Graph(
+      SILValue("%6") ~> SILValue("%5"),
       SILValue("%5") ~> SILValue("%4"),
-      SILValue("%4") ~> SILValue("%3"),
-      SILValue("%4") ~> SILValue("%2"),
-      SILValue("%2") ~> SILValue("%0"),
-      SILValue("%1") ~> SILValue("%0")
+      SILValue("%5") ~> SILValue("%3"),
+      SILValue("%3") ~> SILValue("%1"),
+      SILValue("%2") ~> SILValue("%1"),
+      SILValue("%0")
     ))
 
-    usage.unusedValues should be(Set(SILValue("%1")))
+    usage.unusedValues should be(Set(SILValue("%2")))
+    usage.unusedArgs should be(Set(SILValue("%0")))
   }
 }
