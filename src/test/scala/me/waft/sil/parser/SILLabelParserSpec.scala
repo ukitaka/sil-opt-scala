1df868361c2a84c477abf421aa46b0d0d13291ab
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-11 13:36:18 +0900

    Add SILLabelParserSpec

---
+++ src/test/scala/me/waft/sil/parser/SILLabelParserSpec.scala
@@ -0,0 +1,11 @@
+package me.waft.sil.parser
+
+import org.scalatest._
+
+class SILLabelParserSpec extends FlatSpec with Matchers with SILLabelParser {
+  "SIL label" should "be parsed well" in {
+    val sil = "bb1(%3 : $Builtin.Int32, %4 : $Builtin.Int32):"
+    val label = silLabel.parse(sil).get.value
+    label.identifier shouldBe("bb1")
+  }
+}
