2bffca27ba7521862a08ec250a23107c10e12740
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-11 11:23:01 +0900

    Add DCE spec for "@dead1"

--- src/test/scala/me/waft/sil/optimizer/pass/DCESpec.scala
+++ src/test/scala/me/waft/sil/optimizer/pass/DCESpec.scala
@@ -27,4 +27,32 @@ class DCESpec extends FlatSpec with Matchers with SILFunctionParser {
     bb.instructionDefs.exists(_.values.contains(SILValue("%0"))) should be(true)
     optimizedBb.instructionDefs.exists(_.values.contains(SILValue("%0"))) should be(false)
   }
+
+  "@dead1" should "be optimized well" in {
+    val sil =
+      """sil @dead1 : $@convention(thin) (Int32, Int32) -> Int32 {
+        |bb0(%0 : $Int32, %1 : $Int32):
+        |  %3 = struct_extract %0 : $Int32, #Int32._value
+        |  %4 = struct_extract %1 : $Int32, #Int32._value
+        |  %5 = integer_literal $Builtin.Int1, -1
+        |  %6 = builtin "sadd_with_overflow_Int32"(%3 : $Builtin.Int32, %4 : $Builtin.Int32, %5 : $Builtin.Int1) : $(Builtin.Int32, Builtin.Int1)
+        |  %7 = tuple_extract %6 : $(Builtin.Int32, Builtin.Int1), 0
+        |  %8 = struct $Int32 (%7 : $Builtin.Int32)
+        |  return %0 : $Int32
+        |}
+      """.stripMargin
+
+    val optimizedSil =
+      """sil @dead1 : $@convention(thin) (Int32, Int32) -> Int32 {
+        |bb0(%0 : $Int32, %1 : $Int32):
+        |  return %0 : $Int32
+        |}
+       """.stripMargin
+
+    val bb0 = silFunction.parse(sil).get.value.basicBlocks.head
+    val bb1 = silFunction.parse(optimizedSil).get.value.basicBlocks.head
+    val bb2 = DCE.eliminateDeadCodeInBB(bb0)
+
+    bb1 shouldBe(bb2)
+  }
 }
