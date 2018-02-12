943f6ee3ca91257d34c255078a08e348a59b2fc3
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-12 18:44:56 +0900

    Impl dominators

--- src/main/scala/me/waft/sil/optimizer/meta/DominatorTree.scala
+++ src/main/scala/me/waft/sil/optimizer/meta/DominatorTree.scala
@@ -4,12 +4,37 @@ import me.waft.sil.lang.SILBasicBlock
 
 import scalax.collection.GraphEdge
 import scalax.collection.GraphPredef._
+import scalax.collection.GraphTraversal.DepthFirst
 import scalax.collection.immutable.Graph
 
-class DominatorTree(controlFlowGraph: CFG) {
+class DominatorTree(cfg: CFG) {
   import Implicits._
+  import scala.collection.mutable.Map
 
-  case class Leveled[T](value: T, level: Int)
+  type G = Graph[SILBasicBlock, GraphEdge.DiEdge]
+  type D = Map[SILBasicBlock, Set[SILBasicBlock]]
 
-  type G = Graph[Leveled[SILBasicBlock], GraphEdge.DiEdge]
+  private var d: D = Map(cfg.entryNode.value -> Set(cfg.entryNode.value))
+
+  cfg.entryNode
+    .innerNodeTraverser
+    .withKind(DepthFirst)
+    .filterNot(_ == cfg.entryNode)
+    .foreach { node =>
+      val bb = node.value
+      val predecessors: Set[SILBasicBlock] = node.diPredecessors.map(_.value)
+      val unionPredecessorsD = predecessors
+        .map { predecessor =>
+          d.get(predecessor.value) match {
+            case Some(s) => s
+            case None => {
+              d.update(predecessor.value, cfg.graph.nodes.map(_.value).toSet)
+              d(predecessor.value)
+            }
+          }
+        }
+        .reduce(_ union _)
+      d.update(bb, Set(bb) intersect unionPredecessorsD)
+    }
+  println(d)
 }
