3e9d0838311bf2a75b29b6b9a005ed823770b48a
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-12 16:20:09 +0900

    Add graph spec

--- src/test/scala/me/waft/sil/optimizer/meta/GraphSpec.scala
+++ src/test/scala/me/waft/sil/optimizer/meta/GraphSpec.scala
@@ -26,4 +26,36 @@ class GraphSpec extends FlatSpec with Matchers {
     g.filter(g.having(node = _ == 2)) should be(Graph(2))
     g.filter(g.having(edge = _ contains 2)) should be(Graph(3~>2, 4~>2, 5~>2))
   }
+
+  "graph 2" should "work well" in {
+    val g = Graph(
+      "r" ~> "1",
+      "r" ~> "exit",
+      "1" ~> "2",
+      "2" ~> "3",
+      "2" ~> "4",
+      "3" ~> "5",
+      "3" ~> "6",
+      "4" ~> "exit",
+      "5" ~> "7",
+      "6" ~> "7",
+      "7" ~> "2"
+    )
+
+    val node1 = g.get("1")
+    val node2 = g.get("2")
+    val node3 = g.get("3")
+
+    node1.hasPredecessors shouldBe(true)
+    node1.diPredecessors shouldBe(Set("r"))
+    node1.hasSuccessors shouldBe(true)
+    node1.diSuccessors shouldBe(Set("2"))
+    node1.neighbors shouldBe(Set("r", "2"))
+
+    node2.hasPredecessors shouldBe(true)
+    node2.diPredecessors shouldBe(Set("1", "7"))
+    node2.hasSuccessors shouldBe(true)
+    node2.diSuccessors shouldBe(Set("3", "4"))
+
+  }
 }
