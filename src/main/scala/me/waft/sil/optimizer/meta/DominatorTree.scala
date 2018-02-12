06ca5b1b5f64a3087d0ef9c2cafdc0d2b999bd3b
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-12 20:45:46 +0900

    Add DominatorTreeSpec

--- src/main/scala/me/waft/sil/optimizer/meta/DominatorTree.scala
+++ src/main/scala/me/waft/sil/optimizer/meta/DominatorTree.scala
@@ -7,14 +7,14 @@ import scalax.collection.GraphPredef._
 import scalax.collection.GraphTraversal.DepthFirst
 import scalax.collection.immutable.Graph
 
-class DominatorTree(cfg: CFG) {
+case class DominatorTree(cfg: CFG) {
   import Implicits._
   import scala.collection.mutable.Map
 
   type G = Graph[SILBasicBlock, GraphEdge.DiEdge]
   type D = Map[SILBasicBlock, Set[SILBasicBlock]]
 
-  private var d: D = Map(cfg.entryNode.value -> Set(cfg.entryNode.value))
+  private val d: D = Map(cfg.entryNode.value -> Set(cfg.entryNode.value))
 
   cfg.entryNode
     .innerNodeTraverser
@@ -23,7 +23,7 @@ class DominatorTree(cfg: CFG) {
     .foreach { node =>
       val bb = node.value
       val predecessors: Set[SILBasicBlock] = node.diPredecessors.map(_.value)
-      val unionPredecessorsD = predecessors
+      val intersectedPredecessorsD = predecessors
         .map { predecessor =>
           d.get(predecessor.value) match {
             case Some(s) => s
@@ -33,8 +33,12 @@ class DominatorTree(cfg: CFG) {
             }
           }
         }
-        .reduce(_ union _)
-      d.update(bb, Set(bb) intersect unionPredecessorsD)
+        .reduce(_ intersect  _)
+      println(s"${node.label.identifier}'s pred is ${predecessors.map(_.label.identifier)}")
+      println(s"union of ${intersectedPredecessorsD.map(_.label.identifier)} and ${Set(bb.label.identifier)} is...")
+      println((Set(bb) union intersectedPredecessorsD).map(_.label.identifier))
+      println("------")
+      d.update(bb, Set(bb) union  intersectedPredecessorsD)
     }
-  println(d)
+  println(d.map(kv => (kv._1.label.identifier, kv._2.map(_.label.identifier))))
 }
