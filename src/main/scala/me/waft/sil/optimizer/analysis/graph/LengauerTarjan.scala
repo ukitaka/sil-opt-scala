3ffd197e508e407d905d9dcd454352be05043794
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-13 19:43:30 +0900

    Add dfst

--- src/main/scala/me/waft/sil/optimizer/analysis/graph/LengauerTarjan.scala
+++ src/main/scala/me/waft/sil/optimizer/analysis/graph/LengauerTarjan.scala
@@ -2,6 +2,9 @@ package me.waft.sil.optimizer.analysis.graph
 
 import scalax.collection.{Graph, GraphEdge}
 
-case class LengauerTarjan[N](graph: Graph[N, GraphEdge.DiEdge]) {
+case class LengauerTarjan[N](graph: Graph[N, GraphEdge.DiEdge], entryNodeValue: N) {
   type NodeT = graph.NodeT
+
+  private lazy val depthFirstSpanningTree = DepthFirstSpanningTree(graph, entryNodeValue)
+
 }
