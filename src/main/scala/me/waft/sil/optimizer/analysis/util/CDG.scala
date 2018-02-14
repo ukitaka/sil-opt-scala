440c03a095c1988b33eadc4d240390e6ffff52eb
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-14 18:31:43 +0900

    Add trasform of dominator tree

---
+++ src/main/scala/me/waft/sil/optimizer/analysis/util/CDG.scala
@@ -0,0 +1,11 @@
+package me.waft.sil.optimizer.analysis.util
+
+import me.waft.sil.optimizer.analysis.CFG
+
+case class CDG[N](graph: DiGraph[N], entryNodeValue: N) {
+
+}
+
+//object CDG {
+//  def from(cfg: CFG): CDG[] = ???
+//}
--- src/main/scala/me/waft/sil/optimizer/analysis/util/Transform.scala
+++ src/main/scala/me/waft/sil/optimizer/analysis/util/Transform.scala
@@ -14,4 +14,12 @@ object Transform {
 
   def postDominatorTree[N](graph: Graph[N, GraphEdge.DiEdge], exitNodeValue: N): Graph[N, GraphEdge.DiEdge] =
     dominatorTree(reverse(graph), exitNodeValue)
+
+  def dominanceFrontier[N](graph: Graph[N, GraphEdge.DiEdge], entryNodeValue: N): Map[N, Set[N]] = {
+    val df = DominanceFrontier(graph, entryNodeValue)
+    graph.nodes.map(node => Map(node.value -> df.computeDF(node.value))).reduce(_ ++ _)
+  }
+
+  def postDominanceFrontier[N](graph: Graph[N, GraphEdge.DiEdge], exitNodeValue: N): Map[N, Set[N]] =
+    dominanceFrontier(reverse((graph)), exitNodeValue)
 }
