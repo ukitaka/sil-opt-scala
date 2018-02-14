e98eddd4272d04bc9d000e914e281621df01e851
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-14 18:05:42 +0900

    Implement dfLocal

--- src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
+++ src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
@@ -2,4 +2,17 @@ package me.waft.sil.optimizer.analysis.util
 
 case class DominanceFrontier[N](graph: DiGraph[N], entryNodeValue: N) {
 
+  type NodeT = graph.NodeT
+
+  val dominatorTree = LengauerTarjan(graph, entryNodeValue).dominatorTree
+
+  def dfLocal(n: N): Set[N] = {
+    val node = graph.get(n)
+    val dtNode = dominatorTree.get(n)
+    node.diSuccessors
+      .filterNot { s =>
+        dtNode.pathTo(dominatorTree.get(s)).isDefined && s.value != dtNode.value
+      }
+      .map(_.value)
+  }
 }
