e1b6dcff3ae97c74295bac656bcddcf11f9a7c90
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-14 18:14:03 +0900

    Fix logic

--- src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
+++ src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
@@ -4,15 +4,18 @@ case class DominanceFrontier[N](graph: DiGraph[N], entryNodeValue: N) {
 
   type NodeT = graph.NodeT
 
-  val dominatorTree = LengauerTarjan(graph, entryNodeValue).dominatorTree
+  val lt = LengauerTarjan(graph, entryNodeValue)
 
   def dfLocal(n: N): Set[N] = {
     val node = graph.get(n)
-    val dtNode = dominatorTree.get(n)
     node.diSuccessors
-      .filterNot { s =>
-        dtNode.pathTo(dominatorTree.get(s)).isDefined && s.value != dtNode.value
+      .filter { y =>
+        lt.immediateDominator(y).value != n
       }
       .map(_.value)
+  }
+
+  def dfUp(n: N): Set[N] = {
+    ???
   }
 }
