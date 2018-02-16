c70c96a90f3bd4bc4091e0d615d94a27fa2729b0
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-16 15:49:28 +0900

    Remove unused import

--- src/main/scala/me/waft/sil/lang/SILBasicBlock.scala
+++ src/main/scala/me/waft/sil/lang/SILBasicBlock.scala
@@ -1,7 +1,5 @@
 package me.waft.sil.lang
 
-import me.waft.sil.lang.Br
-
 case class SILBasicBlock(label: SILLabel, instructionDefs: Seq[SILInstructionDef], terminator: SILTerminator) {
   val statements: Seq[SILStatement] =
     instructionDefs.map(i => SILStatement(i, this)) :+ SILStatement(terminator, this)
