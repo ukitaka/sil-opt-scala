f79dd9d7f8a2a43f397f9bdcb016751881e4024d
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-17 10:54:23 +0900

    Remove unnecessary spec

--- src/test/scala/me/waft/sil/optimizer/pass/theory/DCESpec.scala
+++ src/test/scala/me/waft/sil/optimizer/pass/theory/DCESpec.scala
@@ -5,31 +5,6 @@ import me.waft.sil.parser.SILFunctionParser
 import org.scalatest._
 
 class DCESpec extends FlatSpec with Matchers with SILFunctionParser {
-  "Dead code elimination pass" should "work well" in {
-    val sil =
-      """sil hidden @_T01d8hogehgoeSiyF : $@convention(thin) () -> Int {
-        |bb0:
-        |  %0 = integer_literal $Builtin.Int64, 1          // user: %1
-        |  %1 = struct $Int (%0 : $Builtin.Int64)          // user: %2
-        |  %3 = integer_literal $Builtin.Int64, 2          // user: %4
-        |  %4 = struct $Int (%3 : $Builtin.Int64)          // user: %5
-        |  return %4 : $Int                                // id: %5
-        |}
-      """.stripMargin
-    val func = silFunction.parse(sil).get.value
-    val bb = func.basicBlocks.head
-    val optimizedFunc = DCE.run(func)
-    val optimizedBb = optimizedFunc.basicBlocks.head
-
-    // %1 is eliminated
-    bb.instructionDefs.exists(_.values.contains(SILValue("%1"))) should be(true)
-    optimizedBb.instructionDefs.exists(_.values.contains(SILValue("%1"))) should be(false)
-
-    // and %0 is eliminated
-    bb.instructionDefs.exists(_.values.contains(SILValue("%0"))) should be(true)
-    optimizedBb.instructionDefs.exists(_.values.contains(SILValue("%0"))) should be(false)
-  }
-
   "@dead1" should "be optimized well" in {
     val sil =
       """sil @dead1 : $@convention(thin) (Int32, Int32) -> Int32 {
