7307b30354fb4025702ec8c7580d893b8edee7f6
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-19 22:05:47 +0900

    Fix bug

--- src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
+++ src/main/scala/me/waft/sil/optimizer/analysis/util/DominanceFrontier.scala
@@ -1,13 +1,23 @@
 package me.waft.sil.optimizer.analysis.util
 
+import scala.collection.mutable.{Map => MutableMap}
+
 case class DominanceFrontier[N](graph: DiGraph[N], entryNodeValue: N) {
 
   type NodeT = graph.NodeT
 
+  private val _df = MutableMap[N, Set[N]]()
+
   private val lt = LengauerTarjan(graph, entryNodeValue)
   private val dt = lt.dominatorTree
 
-  def computeDF(n: N): Set[N] = dfLocal(n) union dfUp(n)
+  def computeDF(n: N): Set[N] =
+    if (_df.contains(n)) {
+      _df(n)
+    } else {
+      _df(n) = dfLocal(n) union dfUp(n)
+      _df(n)
+    }
 
   def dfLocal(n: N): Set[N] = {
     val node = graph.get(n)
@@ -24,10 +34,7 @@ case class DominanceFrontier[N](graph: DiGraph[N], entryNodeValue: N) {
       .flatMap { c =>
         val dfc = computeDF(c.value)
         dfc.filter { w =>
-          // Here is described as "n does not dominates w"
-          // in "Modern Compiler Implementation in ML",
-          // "c does not dominates w" to be exact.
-          c.pathTo(dt.get(w)).isEmpty
+          n == w || node.pathTo(dt.get(w)).isEmpty
         }
       }
   }
--- src/test/scala/me/waft/sil/optimizer/analysis/util/CDGSpec.scala
+++ src/test/scala/me/waft/sil/optimizer/analysis/util/CDGSpec.scala
@@ -14,13 +14,13 @@ class CDGSpec extends FlatSpec with Matchers {
 
   "Control Dependence Graph" should "be computed well from SSA program 2" in {
     val g = Graph(1 ~> 2, 2 ~> 3, 2 ~> 4, 3 ~> 5, 3 ~> 6, 5 ~> 7, 6 ~> 7, 7 ~> 2)
-    println(GraphTransformer.postDominatorTree(g,  4))
     val cdg = GraphTransformer.controlDependenceGraph(g, 1, 4, 0)
-    cdg should be(Graph(2 ~> 2, 2 ~> 3, 2 ~> 7, 3 ~> 5, 3 ~> 6))
+    cdg should be(Graph(1, 4, 2 ~> 2, 2 ~> 3, 2 ~> 7, 3 ~> 5, 3 ~> 6))
   }
-//  "Control Dependence Graph" should "be computed well from SSA program 3" in {
-//    val g = Graph(0 ~> 1, 1 ~> 2, 1 ~> 4, 2 ~> 3, 3 ~> 4)
-//    val cdg = GraphTransformer.controlDependenceGraph(g, 0, 4, -1)
-//    cdg should be(Graph(0, 1, 2, 3, 4, 1 ~> 2, 1 ~> 3))
-//  }
+
+  "Control Dependence Graph" should "be computed well from SSA program 3" in {
+    val g = Graph(0 ~> 1, 1 ~> 2, 1 ~> 4, 2 ~> 3, 3 ~> 4)
+    val cdg = GraphTransformer.controlDependenceGraph(g, 0, 4, -1)
+    cdg should be(Graph(0, 1, 2, 3, 4, 1 ~> 2, 1 ~> 3))
+  }
 }
