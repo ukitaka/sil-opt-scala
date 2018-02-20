ed8840fa79d261c3cc83fb07bf601f14c4dcfd99
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-20 23:53:24 +0900

    Implement eliminatation of instruction def

--- src/main/scala/me/waft/sil/optimizer/pass/CSE.scala
+++ src/main/scala/me/waft/sil/optimizer/pass/CSE.scala
@@ -1,7 +1,32 @@
 package me.waft.sil.optimizer.pass
 
-import me.waft.sil.lang.SILFunction
+import me.waft.sil.lang._
+import me.waft.sil.optimizer.analysis.SILFunctionAnalysis
+
+case class CSE(function: SILFunction) {
+  type Available = Map[SILInstruction, SILValue]
+
+  lazy val analysis = SILFunctionAnalysis(function)
+
+  def eliminateCommonSubexpression: SILFunction = ???
+
+  def eliminate(bb: SILBasicBlock,
+                available: Available): (SILBasicBlock, Available) = ???
+
+  def eliminate(instructionDef: SILInstructionDef,
+                available: Available): (Option[SILInstructionDef], Available) =
+    if (available.contains(instructionDef.instruction)) {
+      (None, available)
+    } else {
+      (Some(instructionDef),
+       available ++ instructionDef.values
+         .map(v => Map(instructionDef.instruction -> v))
+         .reduce(_ ++ _))
+    }
+
+}
 
 object CSE extends SILFunctionTransform {
-  def run(function: SILFunction): SILFunction = function
+  def run(function: SILFunction): SILFunction =
+    CSE(function).eliminateCommonSubexpression
 }
