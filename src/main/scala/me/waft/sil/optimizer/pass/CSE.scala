2af3d3698dd4dedd99d8a04144ca477f1968f53f
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-21 17:39:52 +0900

    Organize logic and add `canHandle` method to prevent to eliminate useful instruction

--- src/main/scala/me/waft/sil/optimizer/pass/CSE.scala
+++ src/main/scala/me/waft/sil/optimizer/pass/CSE.scala
@@ -9,44 +9,107 @@ case class CSE(function: SILFunction) {
   type Available = Map[SILInstruction, SILValue]
   type Replace = Map[SILValue, SILValue]
 
+  case class EliminationInfo(available: Available, replace: Replace) {
+    def addToReplace(instructionDef: SILInstructionDef) =
+      EliminationInfo(available,
+                      replace ++ instructionDef.values
+                        .map(_ -> available(instructionDef.instruction))
+                        .toMap)
+
+    def makeAvailable(instructionDef: SILInstructionDef): EliminationInfo =
+      EliminationInfo(
+        available ++ instructionDef.values
+          .map(v => instructionDef.instruction -> v)
+          .toMap,
+        replace
+      )
+  }
+
+  object EliminationInfo {
+    def empty: EliminationInfo = EliminationInfo(Map(), Map())
+  }
+
   lazy val analysis = SILFunctionAnalysis(function)
 
   def eliminateCommonSubexpression: SILFunction = {
-    val initialAvailable: Available = Map()
-    val initialReplace: Replace = Map()
-    val (_, replace) = function.basicBlocks.foldLeft(initialAvailable, initialReplace) {
-      case ((a, r), bb) =>
-        eliminate(bb, a, r)
-    }
+    val info = function.basicBlocks.foldLeft(EliminationInfo.empty)(eliminate)
     val optimized = function
-      .filterInstructionDef(i => !i.values.forall(replace.contains))
-      .mapBB(bb => SILValueReplacer.replaceValuesInBasicBlock(bb)(v => replace.getOrElse(v, v)))
+      .filterInstructionDef(i => !i.values.forall(info.replace.contains))
+      .mapBB(bb =>
+        SILValueReplacer.replaceValuesInBasicBlock(bb)(v =>
+          info.replace.getOrElse(v, v)))
     SILFunctionValueRenamer.renameValues(optimized)
   }
 
-  def eliminate(bb: SILBasicBlock,
-                available: Available,
-                replace: Replace): (Available, Replace) =
-    bb.instructionDefs.foldLeft(available, replace) {
-      case ((a, r), i) =>
-        eliminate(i, a, r)
-    }
+  def eliminate(info: EliminationInfo, bb: SILBasicBlock): EliminationInfo =
+    bb.instructionDefs.foldLeft(info)(eliminate)
 
-  def eliminate(instructionDef: SILInstructionDef,
-                available: Available,
-                replace: Replace): (Available, Replace) =
-    if (available.contains(instructionDef.instruction)) {
-      (available,
-       replace ++ instructionDef.values
-         .map(_ -> available(instructionDef.instruction))
-         .toMap)
+  def eliminate(info: EliminationInfo,
+                instructionDef: SILInstructionDef): EliminationInfo =
+    if (canHandle(instructionDef)) {
+      if (info.available.contains(instructionDef.instruction)) {
+        info.addToReplace(instructionDef)
+      } else {
+        info.makeAvailable(instructionDef)
+      }
     } else {
-      (available ++ instructionDef.values
-         .map(v => instructionDef.instruction -> v)
-         .toMap,
-       replace)
+      info
     }
 
+  def canHandle(instructionDef: SILInstructionDef): Boolean =
+    instructionDef.instruction match {
+      //    case ClassMethod => true
+      //    case SuperMethod => true
+      //    case FunctionRef => true
+      //    case GlobalAddr => true
+      case IntegerLiteral(_, _) => true
+      //    case FloatLiteral => true
+      //    case StringLiteral => true
+      case Struct(_, _)        => true
+      case StructExtract(_, _) => true
+      //    case StructElementAddr => true
+      case Tuple(_)           => true
+      case TupleExtract(_, _) => true
+      //    case TupleElementAddr => true
+      //    case Metatype => true
+      //    case ValueMetatype => true
+      //    case ObjCProtocol => true
+      //    case RefElementAddr => true
+      //    case RefTailAddr => true
+      case ProjectBox(_) => true
+      //    case IndexRawPointer => true
+      //    case IndexAddr => true
+      //    case PointerToAddress => true
+      //    case AddressToPointer => true
+      //    case CondFail => true
+      //    case Enum => true
+      //    case UncheckedEnumData => true
+      //    case UncheckedTrivialBitCast => true
+      //    case UncheckedBitwiseCast => true
+      //    case RefToRawPointer => true
+      //    case RawPointerToRef => true
+      //    case RefToUnowned => true
+      //    case UnownedToRef => true
+      //    case RefToUnmanaged => true
+      //    case UnmanagedToRef => true
+      //    case Upcast => true
+      //    case ThickToObjCMetatype => true
+      //    case ObjCToThickMetatype => true
+      //    case UncheckedRefCast => true
+      //    case UncheckedAddrCast => true
+      //    case ObjCMetatypeToObject => true
+      //    case ObjCExistentialMetatypeToObject => true
+      //    case SelectEnum => true
+      //    case SelectValue => true
+      //    case RefToBridgeObject => true
+      //    case BridgeObjectToRef => true
+      //    case BridgeObjectToWord => true
+      //    case ThinFunctionToPointer => true
+      //    case PointerToThinFunction => true
+      //    case MarkDependence => true
+      //    case OpenExistentialRef => true
+      case _ => false
+    }
 }
 
 object CSE extends SILFunctionTransform {
