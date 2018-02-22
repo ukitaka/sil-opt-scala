d9d81db31f0573b9e68139be5c8d0eec5d2c341a
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-22 23:21:12 +0900

    Add open_existential_{addr|value|ref|metatype} parser

--- src/main/scala/me/waft/sil/parser/SILInstructionParser.scala
+++ src/main/scala/me/waft/sil/parser/SILInstructionParser.scala
@@ -5,13 +5,14 @@ import me.waft.core.parser.Parser
 import me.waft.sil.lang.{SILInstruction, SILInstructionDef, SILValue}
 
 trait SILInstructionParser
-  extends Parser
+    extends Parser
     with SILValueParser
     with instruction.AllocParser
     with instruction.AggregateTypesParser
     with instruction.LiteralParser
     with instruction.FunctionApplicationParser
     with instruction.AccessingMemoryParser
+    with instruction.ProtocolParser
     with instruction.ReferenceCountingParser {
 
   import WhiteSpaceApi._
@@ -28,6 +29,8 @@ trait SILInstructionParser
       .map(SILInstructionDef.tupled)
 
   def silInstruction: P[SILInstruction] =
-    allocStack | allocBox | struct | structExtract | integerLiteral | builtin | projectBox | store | load |
-      strongRelease | tuple | tupleExtract
+    allocStack | allocBox | struct | structExtract | integerLiteral |
+      builtin | projectBox | store | load |
+      strongRelease | tuple | tupleExtract |
+      openExistentialAddr | openExistentialValue | openExistentialRef | openExistentialMetatype
 }
---
+++ src/main/scala/me/waft/sil/parser/instruction/ProtocolParser.scala
@@ -0,0 +1,28 @@
+package me.waft.sil.parser.instruction
+
+import fastparse.noApi._
+import me.waft.sil.lang._
+import me.waft.sil.parser.SILOperandParser
+
+trait ProtocolParser extends SILOperandParser {
+  import WhiteSpaceApi._
+
+  private def silAllowedAccess: P[SILAllowedAccess] =
+    ("mutable_access" | "immutable_access").!.map(SILAllowedAccess.apply)
+
+  def openExistentialAddr: P[OpenExistentialAddr] =
+    ("open_existential_addr" ~ silAllowedAccess ~ silOperand ~ "to" ~ silType)
+      .map(OpenExistentialAddr.tupled)
+
+  def openExistentialValue: P[OpenExistentialValue] =
+    ("open_existential_value" ~ silOperand ~ "to" ~ silType)
+      .map(OpenExistentialValue.tupled)
+
+  def openExistentialRef: P[OpenExistentialRef] =
+    ("open_existential_value" ~ silOperand ~ "to" ~ silType)
+      .map(OpenExistentialRef.tupled)
+
+  def openExistentialMetatype: P[OpenExistentialMetatype] =
+    ("open_existential_metatype" ~ silOperand ~ "to" ~ silType)
+      .map(OpenExistentialMetatype.tupled)
+}
