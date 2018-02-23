47cc76e416149d727c79064be7c32521b8120965
ukitaka <yuki.takahashi.1126@gmail.com>
2018-02-23 17:57:37 +0900

    Fix annotated type emittion

--- src/main/scala/me/waft/sil/emitter/SILEmitter.scala
+++ src/main/scala/me/waft/sil/emitter/SILEmitter.scala
@@ -17,7 +17,8 @@ object SILEmitter {
     case TupleType(elements) =>
       "(" + elements.map(emitSwiftType).mkString(", ") + ")"
     case FunctionTypeArgument(_, t) => "(" + emitSwiftType(t) + ")"
-    case AnnotatedType(a, t)        => "(" + emitSwiftType(t) + ")"
+    case AnnotatedType(attributes, t) =>
+      attributes.map(emitSwiftTypeAttribute).mkString(" ") + emitSwiftType(t)
   }
 
   def emitSwiftTypeAttribute(attribute: Attribute): String =
