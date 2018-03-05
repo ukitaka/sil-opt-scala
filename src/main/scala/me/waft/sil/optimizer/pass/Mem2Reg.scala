996f7c462c6258ff69c744d0ad6bfc2be0780ee0
ukitaka <yuki.takahashi.1126@gmail.com>
2018-03-05 22:41:34 +0900

    Implement `isWriteOnlyAllocation`

--- src/main/scala/me/waft/sil/optimizer/pass/Mem2Reg.scala
+++ src/main/scala/me/waft/sil/optimizer/pass/Mem2Reg.scala
@@ -1,11 +1,27 @@
 package me.waft.sil.optimizer.pass
 
-import me.waft.sil.lang.SILFunction
+import me.waft.sil.lang._
+import me.waft.sil.optimizer.analysis.SILFunctionAnalysis
+import me.waft.sil.optimizer.util.Implicits._
 
 case class Mem2Reg(function: SILFunction) {
+  val analysis = SILFunctionAnalysis(function)
 
+  def promote: SILFunction = {
+    function.mapStatement(ss => ss)
+  }
+
+  def isWriteOnlyAllocation(value: SILValue): Boolean =
+    analysis.valueUsage
+      .allImmediateUsers(value)
+      .forall(s =>
+        s.instruction match {
+          case Store(_, _)       => true
+          case DeallocStack(_)   => true
+          case DebugValueAddr(_) => true
+      })
 }
 
 object Mem2Reg {
-  def run(function: SILFunction): SILFunction = ???
+  def run(function: SILFunction): SILFunction = Mem2Reg(function).promote
 }
